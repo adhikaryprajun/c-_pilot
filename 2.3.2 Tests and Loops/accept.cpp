b5b83ee96e93596613edf053c29d558172a6b44b
Prajun <prajun.adhikary@gmail.com>
2018-01-21 10:28:27 +0530

    Checking no of tries

--- 2.3.2 Tests and Loops/accept.cpp
+++ 2.3.2 Tests and Loops/accept.cpp
@@ -8,7 +8,7 @@ bool accept() {
 	if(answer=='y') return true;
 	return false;
 	*/
-	
+
 	switch(answer) {
 		case 'y':
 			return true;
@@ -18,4 +18,25 @@ bool accept() {
 			cout << "I'll take that for a no." << endl;
 			return false;
 	}
+}
+
+bool accept3() {
+	int tries = 1;
+	while(tries < 4) {
+		cout << "Do you want to proceed (y or n)?" << endl;
+		char answer = 0;
+		cin >> answer;
+
+		switch(answer) {
+			case 'y':
+				return true;
+			case 'n':
+				return false;
+			default:
+				cout << "Sorry, I don't understand that." << endl;
+				tries = tries + 1;
+		}
+	}
+	cout << "I'll take that for a no." << endl;
+	return false;
 }
